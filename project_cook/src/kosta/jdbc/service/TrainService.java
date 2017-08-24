fde9113bfdb3fdabc9c2f4d0d9d21ecb96b4cf90
user <user@user-PC>
2017-08-24 15:32:50 +0900

    기차 서비스 완료

--- project_cook/src/kosta/jdbc/service/TrainService.java
+++ project_cook/src/kosta/jdbc/service/TrainService.java
@@ -1,6 +1,7 @@
 package kosta.jdbc.service;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -84,20 +85,23 @@ public class TrainService implements Service {
 //		System.out.println(guestCity);
 		
 		// TODO Auto-generated method stub
-		String guestCityCode = "";
+		String guestStateCode = "";
 		for (int i = 0; i < nodeList.getLength(); i++) {
 			Node node = nodeList.item(i);
 			if(node.getNodeType() == Node.ELEMENT_NODE){
 				Element element = (Element)node;
 				
 				Node cityName = element.getElementsByTagName("cityname").item(0);
-				if (guestCity.equals(cityName.getFirstChild().getTextContent())) {
+				
+//				System.out.println(guestCity);
+//				System.out.println(cityName.getFirstChild().getTextContent());
+				
+				if (guestState.equals(cityName.getFirstChild().getTextContent())) {
 					Node cityC = element.getElementsByTagName("citycode").item(0);
-					guestCityCode = cityC.getFirstChild().getTextContent();
-					System.out.println(guestCityCode);
+					guestStateCode = cityC.getFirstChild().getTextContent();
+//					System.out.println("guestStateCode : " + guestStateCode);
 					break;
 				}
-				
 			}
 		}
 		
@@ -113,24 +117,138 @@ public class TrainService implements Service {
 			cookCity = entry.getValue();
 		}
 		
-		String cookCityCode = "";
+		String cookStateCode = "";
 		for (int i = 0; i < nodeList.getLength(); i++) {
 			Node node = nodeList.item(i);
 			if(node.getNodeType() == Node.ELEMENT_NODE){
 				Element element = (Element)node;
 				
 				Node cityName = element.getElementsByTagName("cityname").item(0);
-				if (cookCity.equals(cityName.getFirstChild().getTextContent())) {
+				if (cookState.equals(cityName.getFirstChild().getTextContent())) {
 					Node cityC = element.getElementsByTagName("citycode").item(0);
-					cookCityCode = cityC.getFirstChild().getTextContent();
+					cookStateCode = cityC.getFirstChild().getTextContent();
 					break;
 				}
-				System.out.println(cookCityCode);
+//				System.out.println("cookStateCode" + cookStateCode);
 			}
 		}
 		
 //		System.out.println(cookState);
 //		System.out.println(cookCity);
+		
+		// guestCity
+		String guestCityCode = "";
+		try {
+			StringBuilder urlBuilder = new StringBuilder(PropertiesUtil.get("trainstation")); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + PropertiesUtil.get("servicekey")); /*Service Key*/
+	        urlBuilder.append("&" + URLEncoder.encode("cityCode","UTF-8") + "=" + URLEncoder.encode(guestStateCode, "UTF-8")); /*시/도ID*/
+	        URL url = new URL(urlBuilder.toString());
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Content-type", "application/json");
+	        
+	        BufferedReader rd;
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+//				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		        DocumentBuilder builder = factory.newDocumentBuilder();
+		        Document document = builder.parse(conn.getInputStream());
+		        Element root = document.getDocumentElement();
+		        Element nodeLi=(Element) root.getChildNodes().item(1);
+		        
+		        NodeList nodeList = root.getElementsByTagName("item");
+//		        System.out.println(nodeList.getLength());
+		  
+				for (int i = 0; i < nodeList.getLength(); i++) {
+					Node node = nodeList.item(i);
+					if(node.getNodeType() == Node.ELEMENT_NODE){
+						Element element = (Element)node;
+						
+						Node cityName = element.getElementsByTagName("nodename").item(0);
+						if (guestCity.equals(cityName.getFirstChild().getTextContent())) {
+							Node cityC = element.getElementsByTagName("nodeid").item(0);
+							guestCityCode = cityC.getFirstChild().getTextContent();
+							break;
+						}
+					}
+				}
+//				System.out.println("guestCity" + guestCity);
+//				System.out.println("guestCityCode" + guestCityCode);
+			} else {
+				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+	        
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// cookCity
+		String cookCityCode = "";
+		try {
+			StringBuilder urlBuilder = new StringBuilder(PropertiesUtil.get("trainstation")); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + PropertiesUtil.get("servicekey")); /*Service Key*/
+	        urlBuilder.append("&" + URLEncoder.encode("cityCode","UTF-8") + "=" + URLEncoder.encode(cookStateCode, "UTF-8")); /*시/도ID*/
+	        URL url = new URL(urlBuilder.toString());
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Content-type", "application/json");
+	        
+	        BufferedReader rd;
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+//				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		        DocumentBuilder builder = factory.newDocumentBuilder();
+		        Document document = builder.parse(conn.getInputStream());
+		        Element root = document.getDocumentElement();
+		        Element nodeLi=(Element) root.getChildNodes().item(1);
+		        
+		        NodeList nodeList = root.getElementsByTagName("item");
+//		        System.out.println(nodeList.getLength());
+		  
+				for (int i = 0; i < nodeList.getLength(); i++) {
+					Node node = nodeList.item(i);
+					if(node.getNodeType() == Node.ELEMENT_NODE){
+						Element element = (Element)node;
+						
+						Node cityName = element.getElementsByTagName("nodename").item(0);
+						if (cookCity.equals(cityName.getFirstChild().getTextContent())) {
+							Node cityC = element.getElementsByTagName("nodeid").item(0);
+							cookCityCode = cityC.getFirstChild().getTextContent();
+							break;
+						}
+					}
+				}
+//				System.out.println("cookCity" + cookCity);
+//				System.out.println("cookCityCode" + cookCityCode);
+			} else {
+				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+	        
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		//
 		try {
@@ -162,17 +280,33 @@ public class TrainService implements Service {
 		        NodeList nodeList = root.getElementsByTagName("item");
 //		        System.out.println(nodeList.getLength());
 		  
-		        
-				for (int i = 0; i < nodeList.getLength(); i++) {
-					Node node = nodeList.item(i);
-					if(node.getNodeType() == Node.ELEMENT_NODE){
-						Element element = (Element)node;
-						
-						Node train = element.getElementsByTagName("traingradename").item(0);
+		        if (nodeList.getLength() == 0) {
+		        	System.out.println(guestCity + "역에서 " + cookCity + "역까지 이용할 수 있는 기차가 없습니다");
+		        } else {
+		        	System.out.println(guestCity + "역에서 " + cookCity + "역까지 다음의 기차를 이용할 수 있습니다");
+		        	for (int i = 0; i < nodeList.getLength(); i++) {
+		        		Node node = nodeList.item(i);
+		        		if(node.getNodeType() == Node.ELEMENT_NODE){
+		        			Element element = (Element)node;
+		        			
+		        			Node train = element.getElementsByTagName("traingradename").item(0);
 //						System.out.println(element.getElementsByTagName("cityname").item(0).getTextContent());
-						System.out.println(train.getFirstChild().getTextContent());
-					}
-				}
+		        			System.out.println(train.getFirstChild().getTextContent());
+		        		}
+		        	}
+		        }
+		        System.out.println();
+//				System.out.println(guestCity + "역에서 " + cookCity + "역까지 다음의 기차를 이용할 수 있습니다");
+//				for (int i = 0; i < nodeList.getLength(); i++) {
+//					Node node = nodeList.item(i);
+//					if(node.getNodeType() == Node.ELEMENT_NODE){
+//						Element element = (Element)node;
+//						
+//						Node train = element.getElementsByTagName("traingradename").item(0);
+////						System.out.println(element.getElementsByTagName("cityname").item(0).getTextContent());
+//						System.out.println(train.getFirstChild().getTextContent());
+//					}
+//				}
 			} else {
 				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
 			}
